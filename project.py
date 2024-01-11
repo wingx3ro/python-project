10340bfbea9a10d4267d93206cfee59bcd667679
wingx3ro <156338342+wingx3ro@users.noreply.github.com>
2024-01-11 18:14:53 +0200

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,16 @@
 def anoigma:
     f1=open("lessons.csv","r")
     f2=open("supervisors.csv","r")
+
+#kleisimo arxeiwn
 def kleisimo:
     f1.close()
     f2.close()
 
+class Mathimata:
+    def _init_(ID, Date, Classroom, Semester, Prof, Nsupervisors):
+        self.ID=
+
+class Supervisors:
+    def _init_(IDsup, Name, Email, Nsup, Restrictions):
+            
